c79be0a55d84040c15a2d7a888a9170a9bd46848
zac [they/them] <zaccolley@fastmail.com>
2022-03-10 00:58:55 +0000

    Add new identity feature

---
+++ blocked-terms.js
@@ -0,0 +1,67 @@
+/*
+
+  Warning. There is a list of blocked words in this document.
+  This is to curb any abuse of the site. Or at least try.
+  If it doesn't work I'll delete it.
+
+  They're lots of slurs, if you'd prefer not read this leave now.
+
+*/
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+window.BLOCKED_TEXT = [
+    "beaner",
+    "beaners",
+    "bimbos",
+    "bulldyke",
+    "coon",
+    "darkie",
+    "faggot",
+    "fucktard",
+    "fudge packer",
+    "fudgepacker",
+    "jail bait",
+    "jailbait",
+    "jigaboo",
+    "jiggaboo",
+    "jiggerboo",
+    "kike",
+    "lolita",
+    "nambla",
+    "negro",
+    "nigga",
+    "nigger",
+    "nig nog",
+    "paki",
+    "raghead",
+    "rape",
+    "slave",
+    "shemale",
+    "slanteye",
+    "towelhead",
+    "tranny",
+  ];
+  
